d08ca27f1e2c780d03fd2040eeaf13c45f266c7b
Jhulia <127256522+Jhuliamq@users.noreply.github.com>
2025-04-14 08:04:21 -0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,88 @@
+const questao = [
+    {
+      question: "O 3 pertence aos conjunto do números pares ?",
+      answers: [
+        { option: "Sim", correct: true},
+        { option: "Não", correct: false},
+      ],
+    },
+    {
+      question: "O número 2 pertence ao conjunto dos números pares ?",
+      answers: [
+        { option: "Não", correct: false},
+        { option: "Sim", correct: true},
+      ],
+    },
+    {
+      question: "Qual dos números não é maior do que 0 ?",
+      answers: [
+        { option: "1", correct: false},
+        { option: "15", correct: false},
+        { option: "-9", correct: true},
+        { option: "6", correct: false},
+      ],
+    },
+    {
+      question: "Qual grupo não possui apenas números pares ?",
+      answers: [
+        { option:"2, 4, 6, 8", correct: false},
+        { option:"2, 8, 6, 2", correct: false},
+        { option:"4, 2, 8, 4", correct: false},
+        { option:"10, 7, 2, 5", correct: true},
+      ],
+    },
+    {
+      question: "Qual o grupo que só possui números impares ?",
+      answers: [
+        { option: "1, 4, 5", correct: false},
+        { option: "11, 9 ,5", correct: true},
+        { option: "1, 9, 4", correct: false },
+        { option: " 4, 8, 34", correct: false},
+      ],
+    },
+    {
+      question: "Qual dos números não está presente no grupo dos naturais ? (Psiu: São aqueles maiores do que 0 ;) )",
+      answers: [
+        { option: "-1 ", correct: true},
+        { option: "56 ", correct: false},
+        { option: "5 ", correct: false},
+        { option: "8 ", correct: false},
+      ],
+    },
+    {
+      question: "Qual dos números não está presente no grupo dos inteiros ? (Psiu: São aqueles que não tem casa decimais ;) )",
+      answers: [
+        { option: "0.5 ", correct: false},
+        { option: "-5 ", correct: true} ,
+        { option: "4.4", correct: false},
+        { option: "2.5 ", correct: false},
+      ],
+    },
+    {
+    question: "A não, contas :O! Qual das contas tem um número natural ?",
+      answers: [
+        { option: "2-3 ", correct: false},
+        { option: "2-1 ", correct: true},
+        { option: "0-0 ", correct: false},
+        { option: "-3 + 2 ", correct: false},
+      ],
+    },
+    {
+      question: "A não, contas denovo :( ! Qual das contas tem um número inteiro ?",
+      answers: [
+        { option: "1 - 0.5 ", correct: false},
+        { option: "5 - 3 ", correct: true},
+        { option: "0.5 - 0 ", correct: false},
+        { option: "0.5 ", correct: false},
+      ],
+    },
+    {
+      question:"Finalmente a última :) ! Qual deles tem: 1 natural, 1 inteiro e 1 negativo?",
+      answers: [
+        { option:"2, 0.5, 8", correct: false},
+        { option:"-2, 5, -1", correct: false},
+        { option:"1, 5, -1", correct: true},
+        { option:"0, 0, 0", correct: false},
+      ],
+    },
+  ];
